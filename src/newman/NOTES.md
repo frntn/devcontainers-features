81c2461b62418a6ccd433f17c21271b633a40ae2
Matthieu FRONTON <matthieu.fronton@capgemini.com>
2023-01-07 16:19:07 +0000

    init newman declaration

---
+++ src/newman/NOTES.md
@@ -0,0 +1,9 @@
+# Pre-requisites
+
+`bash` is required to execute the `install.sh` script.
+
+`npm` is required to install newman.
+
+# Versions
+
+Available versions of the Newman can be found here: https://www.npmjs.com/package/newman?activeTab=versions.
---
+++ src/newman/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -eux
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+export VERSION="${VERSION:-"latest"}"
+export DEBIAN_FRONTEND=noninteractive
+
+# Check if npm is installed
+if ! command_exists npm; then
+    echo -e 'Feature requires npm to be installed. Please use a node-based image'
+    echo -e 'Example images : '
+    echo -e ' - "mcr.microsoft.com/devcontainers/universal:linux" => Multi-purpose developer image / Ubuntu Focal / 11.4 GB'
+    echo -e ' - "mcr.microsoft.com/devcontainers/javascript-node" => Node image / Debian Bullseye / 1.27 GB'
+    exit 1
+fi
+
+
+echo "(*) Installing ${PACKAGE}..."
+
+PACKAGE="newman@${VERSION}"
+npm install -g "$PACKAGE"
+
+echo "Done!"
