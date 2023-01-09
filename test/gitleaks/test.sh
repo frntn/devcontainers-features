51d4f0455bb78c7fd7a5ff7cc46089127ac55b86
Matthieu FRONTON <matthieu.fronton@capgemini.com>
2023-01-09 04:19:22 +0100

    Add test scripts for Gitleaks and Postman

---
+++ test/gitleaks/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Optional: Import test library
+source dev-container-features-test-lib
+
+# Definition specific tests
+check "version" gitleaks version
+
+# Report result
+reportResults
