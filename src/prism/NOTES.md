23bdbaa4e9828cf73612e722d75a57b71053af2c
Matthieu FRONTON <matthieu.fronton@capgemini.com>
2023-01-10 23:34:46 +0100

    Add prism feature for mocking purpose

--- src/prism/NOTES.md
+++ src/prism/NOTES.md
@@ -6,4 +6,4 @@
 
 # Versions
 
-Available versions of `Newman` can be found here: https://www.npmjs.com/package/newman?activeTab=versions.
+Available versions of `Prism` can be found here: https://www.npmjs.com/package/@stoplight/prism-cli?activeTab=versions.
---
+++ src/prism/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eux
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+export DEBIAN_FRONTEND=noninteractive
+export VERSION="${VERSION:-"latest"}"
+export PACKAGE="@stoplight/prism-cli@${VERSION}"
+
+# Check if npm is installed
+if ! command_exists npm; then
+    echo -e 'Feature requires npm to be installed. Please use a node-based image'
+    echo -e 'Example images : '
+    echo -e ' - "mcr.microsoft.com/devcontainers/universal:linux" => Multi-purpose developer image / Ubuntu Focal / 11.4 GB'
+    echo -e ' - "mcr.microsoft.com/devcontainers/javascript-node:0-18" => Node image / Debian Bullseye / 1.27 GB'
+    exit 1
+fi
+
+echo "(*) Installing ${PACKAGE}..."
+
+npm install -g "$PACKAGE"
+
+echo "Done!"
